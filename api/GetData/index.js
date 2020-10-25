20fbb7e09c73168dbfcddfefefdffcd1e9b3e321
Sasha <55252065+Scall01@users.noreply.github.com>
2020-10-25 16:10:33 -0700

    Added initial data

--- api/GetData/index.js
+++ api/GetData/index.js
@@ -1,11 +1,19 @@
 module.exports = async function (context, req) {
     context.log('Sending data from server');
 
+    // Read parking-data
+    const reader = new FileReader();
+    let data;
 
+    reader.onload = function() {
+        data = JSON.parse(reader.result);
+    }
+
+    reader.readAsText('parking-data.json')
 
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: responseMessage,
+        body: {data: data},
         header: { 'Content-Type': 'application/json' }
     };
 }
