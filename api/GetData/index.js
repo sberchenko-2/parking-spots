ebbb8c96d0d1d709cf0874fdfbe04173052b2b0b
Sasha <55252065+Scall01@users.noreply.github.com>
2020-10-25 18:08:47 -0700

    Testing changes

--- api/GetData/index.js
+++ api/GetData/index.js
@@ -2,11 +2,10 @@ module.exports = async function (context, req) {
     context.log('Sending data from server');
 
     // Read parking-data
-    const reader = new FileReader();
+    //const response = await fetch('parking-data.json');
+    //const data = await response.json();
 
-    const response = await fetch('parking-data.json');
-    const data = await response.json();
-
+    const data = "Testing data"
     context.res = {
         // status: 200, /* Defaults to 200 */
         body: data,
