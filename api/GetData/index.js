5a68a711325a8f6a564be5621cf9221095f4fda5
Sasha <55252065+Scall01@users.noreply.github.com>
2020-11-02 09:38:09 -0800

    Added debugging statements

--- api/GetData/index.js
+++ api/GetData/index.js
@@ -2,16 +2,12 @@ module.exports = async function (context, req) {
     context.log('Sending data from server');
 
     // Read parking-data
-    //const parking_data = fetch('parking-data.json');
-    const parking_data = [
-        { name: 'Azure' },
-        { name: 'Sammy' },
-        { name: 'Roscoe' },
-    ];
+    //const response = await fetch('parking-data.json');
+    //const parking_data = await response.json();
 
     context.res = {
         // status: 200,
-        body: {dogs: parking_data},
+        body: {data: window.location.href},
         header: { 'Content-Type': 'application/json' }
     };
 }
