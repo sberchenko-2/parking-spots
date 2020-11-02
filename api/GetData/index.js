ee75b122b6ba4ddd9ad215a2787bef9b8a7cb182
Sasha <55252065+Scall01@users.noreply.github.com>
2020-11-02 09:31:19 -0800

    Potential fix to internal bug

--- api/GetData/index.js
+++ api/GetData/index.js
@@ -2,11 +2,16 @@ module.exports = async function (context, req) {
     context.log('Sending data from server');
 
     // Read parking-data
-    const parking_data = fetch('parking-data.json');
+    //const parking_data = fetch('parking-data.json');
+    const parking_data = [
+        { name: 'Azure' },
+        { name: 'Sammy' },
+        { name: 'Roscoe' },
+    ];
 
     context.res = {
-        status: 200,
-        body: {data: parking_data},
+        // status: 200,
+        body: {dogs: parking_data},
         header: { 'Content-Type': 'application/json' }
     };
 }
