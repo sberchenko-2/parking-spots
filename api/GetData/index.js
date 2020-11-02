3352da47857cf27454114cd67176e2243067545d
Sasha <55252065+Scall01@users.noreply.github.com>
2020-11-02 09:44:49 -0800

    Potential fix to data-fetching bug

--- api/GetData/index.js
+++ api/GetData/index.js
@@ -2,12 +2,8 @@ module.exports = async function (context, req) {
     context.log('Sending data from server');
 
     // Read parking-data
-    //const response = await fetch('parking-data.json');
-    //const parking_data = await response.json();
-
-    const parking_data = [
-        { url: document.URL}
-    ]
+    const response = await fetch('parking-data.json');
+    const parking_data = await response.json();
 
     context.res = {
         // status: 200,
