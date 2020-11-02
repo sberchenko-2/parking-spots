71dc91e4e50799dbc564be7e9d2b5436bf1938ea
Sasha <55252065+Scall01@users.noreply.github.com>
2020-11-02 09:13:34 -0800

    Added internal debugging statements

--- api/GetData/index.js
+++ api/GetData/index.js
@@ -1,10 +1,14 @@
 module.exports = async function (context, req) {
+    console.log('in internal index');
+
     context.log('Sending data from server');
 
     // Read parking-data
     const response = await fetch('parking-data.json');
+    console.log('obtained response data')
+    console.log(response)
     const parking_data = await response.json();
-
+    console.log('converted data to json')
     console.log(parking_data)
 
     context.res = {
