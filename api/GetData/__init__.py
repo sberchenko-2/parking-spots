bb108dfca36e3efdc9e7e7dd1d6857354c1759ba
Sasha <55252065+Scall01@users.noreply.github.com>
2020-11-14 16:48:40 -0800

    Changed backend to send data from Cosmos database

--- api/GetData/__init__.py
+++ api/GetData/__init__.py
@@ -1,10 +1,32 @@
 import azure.functions as func
+from azure.cosmos import exceptions, CosmosClient, PartitionKey
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
 
-    with open('parking-data.json', 'r') as f:
-        data = ''
-        for line in f.readlines():
-            data += line
-        return func.HttpResponse(data)
+    # Initialize the Cosmos client
+    endpoint = "https://parking-spots.documents.azure.com:443/"
+    key = 'c9av0FP6xBrnz7XNUdBNIUFdCuo1IvE3H2SWvvB8Pxo92ALKzfW2gDXxgQlBlyUJtLDiJSZK6Ew8YO8yPNepnA=='
+    client = CosmosClient(endpoint, key)
+
+    # Select database
+    database_name = 'Parking-Data'
+    database = client.create_database_if_not_exists(id=database_name)
+
+    # Select container
+    container_name = 'Items'
+    container = database.create_container_if_not_exists(
+        id=container_name,
+        partition_key=PartitionKey(path="/slots"),
+        offer_throughput=400
+    )
+
+    # Query slots using SQL
+    query = "SELECT * FROM c"
+
+    items = list(container.query_items(
+        query=query,
+        enable_cross_partition_query=True
+    ))
+
+    return func.HttpResponse(items[0])
