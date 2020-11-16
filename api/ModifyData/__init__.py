1e46015be9db3041b80b5c255119ea6d21ba729d
Sasha <55252065+Scall01@users.noreply.github.com>
2020-11-15 19:13:07 -0800

    Cleaning code

--- api/ModifyData/__init__.py
+++ api/ModifyData/__init__.py
@@ -91,7 +91,7 @@ def process_inputs(name, slot_num, days, repeat, curr_week):
     return name, slot_num, days, repeat, curr_week
 
 
-def main(req: func.HttpRequest, msg: func.Out[func.QueueMessage]) -> str:
+def main(req: func.HttpRequest) -> str:
 
     name, slot_num, days, repeat, curr_week = get_params(req)
 
